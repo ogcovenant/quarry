7dd5204498e66596b0fc960665c5824fd1277883
Covenant Ogowale <justcovenant@outlook.com>
2026-05-26 16:48:29 +0100

    feat(web): completed the sidebar nav reaction

--- apps/web/components/dashboard/siderbar.tsx
+++ apps/web/components/dashboard/siderbar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { HugeiconsIcon } from "@hugeicons/react";
 import Logo from "../logo";
 import { HandPointingLeft03Icon } from "@hugeicons/core-free-icons";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+  const pathname = usePathname();
+
   const navItems = [
     { label: "Chat", href: "/dashboard/chat" },
     { label: "Notes", href: "/dashboard/notes" },
@@ -11,31 +16,40 @@ export default function Sidebar() {
 
   const bottomNavItems = [{ label: "Profile", href: "/profile" }];
 
-  const activeItem = navItems[0];
-
   return (
     <div className="p-5 border-r-2 border-r-border w-[10%] min-h-screen flex flex-col">
       <Logo />
+
       <section className="flex flex-1 flex-col justify-between">
         <section className="mt-10">
           <ul className="space-y-2">
-            {navItems.map((item, index) => (
-              <li key={index} className="flex gap-3 items-center">
-                <a href={item.href}>{item.label}</a>
-                {activeItem === item && (
-                  <HugeiconsIcon icon={HandPointingLeft03Icon} />
-                )}
-              </li>
-            ))}
+            {navItems.map((item, index) => {
+              const isActive = pathname === item.href;
+
+              return (
+                <li key={index} className="flex gap-3 items-center">
+                  <a href={item.href}>{item.label}</a>
+
+                  {isActive && <HugeiconsIcon icon={HandPointingLeft03Icon} />}
+                </li>
+              );
+            })}
           </ul>
         </section>
+
         <section className="mb-5">
           <ul className="space-y-2">
-            {bottomNavItems.map((item, index) => (
-              <li key={index}>
-                <a href={item.href}>{item.label}</a>
-              </li>
-            ))}
+            {bottomNavItems.map((item, index) => {
+              const isActive = pathname === item.href;
+
+              return (
+                <li key={index} className="flex gap-3 items-center">
+                  <a href={item.href}>{item.label}</a>
+
+                  {isActive && <HugeiconsIcon icon={HandPointingLeft03Icon} />}
+                </li>
+              );
+            })}
           </ul>
         </section>
       </section>
