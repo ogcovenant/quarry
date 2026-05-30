155cf7b947dcb98ddbb36b96f2643b3d486a0b97
Covenant Ogowale <justcovenant@outlook.com>
2026-05-30 05:55:20 +0100

    feat(api): auth setup

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from 'src/users/users.service';
 
-@Module({})
+@Module({
+  imports: [UsersModule],
+  controllers: [AuthController],
+  providers: [AuthService, UsersService],
+})
 export class AuthModule {}
