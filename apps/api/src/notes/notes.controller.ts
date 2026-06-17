634914ad2ffd405174ffc228042e487cfd6617b4
Covenant Ogowale <justcovenant@outlook.com>
2026-06-17 03:36:45 +0100

    feat(api) note module base api structure

--- apps/api/src/notes/notes.controller.ts
+++ apps/api/src/notes/notes.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { NotesService } from './notes.service';
 
@@ -6,4 +13,29 @@ import { NotesService } from './notes.service';
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
+
+  @Post('/')
+  async createNote() {
+    return this.notesService.createNote();
+  }
+
+  @Get('/')
+  async getAllNotes() {
+    return this.notesService.getAllNotes();
+  }
+
+  @Get('/:uuid')
+  async getSingleNote() {
+    return this.notesService.getSingleNote();
+  }
+
+  @Patch('/:uuid')
+  async updateSingleNote() {
+    return this.notesService.updateSingleNote();
+  }
+
+  @Delete('/:uuid')
+  async deleteSingleNote() {
+    return this.notesService.deleteSingleNote();
+  }
 }
--- apps/api/src/notes/notes.service.ts
+++ apps/api/src/notes/notes.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Notes } from './entities/note.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+  constructor(
+    @InjectRepository(Notes)
+    private readonly notesRepository: Repository<Notes>,
+  ) {}
+
+  async createNote() {}
+
+  async getAllNotes() {}
+
+  async getSingleNote() {}
+
+  async updateSingleNote() {}
+
+  async deleteSingleNote() {}
+}
